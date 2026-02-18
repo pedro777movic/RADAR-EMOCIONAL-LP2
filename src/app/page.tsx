9df81eee8f4fb116dcf78a0a99415fc227f4189a
Bartolomeu Silva <bartolomeuslv25@gmail.com>
2026-02-18 16:20:21 +0000

    Quero a seção "Risco Realmente Baixo" debaixo da seção "Seu Radar Emocio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,8 +17,8 @@ export default function Home() {
       <MechanismSection />
       <BenefitsSection />
       <TargetAudienceSection />
-      <GuaranteeSection />
       <CtaSection />
+      <GuaranteeSection />
       <Footer />
     </div>
   );
